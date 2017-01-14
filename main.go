c8c9165162367c25e6593e2ee0679c471da60acb
Peter <wangtao9604@gmail.com>
2017-01-14 17:59:32 +0800

    add main.go

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Golang!")
+	fmt.Println("Hello Golang!")
+	fmt.Println("Hello Peter!")
+	fmt.Println("GitHub ast!")
+
+	fmt.Println("Issue1 test1!")
+	fmt.Println("Issue1 test2!")
+}
