964ee2b4c197e87bd203eded25cf47ec0681beef
Peter Yan <84383139@qq.com>
2017-01-16 10:08:14 +0800

    back to the first time commit

--- main.go
+++ main.go
@@ -4,10 +4,4 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello Golang!")
-	fmt.Println("Hello Golang!")
-	fmt.Println("Hello Peter!")
-	fmt.Println("GitHub ast!")
-
-	fmt.Println("Issue1 test1!")
-	fmt.Println("Issue1 test2!")
 }
